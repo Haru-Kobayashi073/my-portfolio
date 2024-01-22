070719f6b87524a14d421deb4aa040193f0d78e8
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-01-22 17:36:11 +0900

    add: create header component frame

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+            <header>
+                <ul>
+                    <li><a href="">Top</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Career</a></li>
+                    <li><a href="">Social</a></li>
+                </ul>
+            </header>
+    );
+};
+
+export default Header;
