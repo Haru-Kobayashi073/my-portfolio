6b67fbac320de11b3024d0c7247c5f21a0b1af68
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-01-23 00:58:43 +0900

    add: create Top component template

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Top from "./components/Top";
 
 function App() {
   return (
     <>
       <Header/>
+      <Top/>
     </>
   );
 }
---
+++ src/components/Top.jsx
@@ -0,0 +1,10 @@
+const Top = () => {
+    return (
+        <div className="top-box">
+            <h1>Top</h1>
+            <p>Top page</p>
+        </div>
+    );
+}
+
+export default Top;
